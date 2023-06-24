a662f2e26959e5ff0e3dfbb8b19c3eb68b228bfc
Matvey Korotygin <matveykaklanter@mail.ru>
2023-06-25 01:04:19 +0200

    Seed default roles

---
+++ src/Infrastructure/Migrations/20230624223247_SeedRoles.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NoteIt.Domain.Entities;
+
+#nullable disable
+
+namespace NoteIt.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { Guid.NewGuid().ToString(), "administrator" },
+                    { Guid.NewGuid().ToString(), "user" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM AspNetRoles
+                                   WHERE Name = administrator");
+
+            migrationBuilder.Sql(@"DELETE FROM AspNetRoles
+                                   WHERE Name = user");
+        }
+    }
+}
